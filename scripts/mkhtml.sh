c597a04a02d756723ee6088f4c865749b9319a88
Stuart Norcross <stuart.norcross@st-andrews.ac.uk>
2021-10-25 10:07:49 +0100

    renamed files and fixed html in mkhtml.sh

--- scripts/mkhtml.sh
+++ scripts/mkhtml.sh
@@ -13,7 +13,7 @@ formatted_text=""
 add_element() {
 		tree_name="Some tree"
 		tree_fact="Some text about this tree goes here"
-		tree_picture_path="A picture of this tree"
+		tree_picture_html="A picture of this tree"
 		copyright="Copyright notice goes here"
 		link="Link to the pdf file about locations of this tree in Dundee"
 		link_text="Text for the PDF link"
@@ -21,8 +21,8 @@ add_element() {
 
 			<h2>$tree_name</h2>
 			<p>$tree_fact</p>
-			$tree_picture_link
-			$copyright
+			$tree_picture_html
+			<p>$copyright</p>
 			<p><a href="$link">$link_text</a></p>
 		  
 			_END_
